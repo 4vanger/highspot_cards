dadf52387bf3fbbd9e42f2b714c01fd6351b715d
Andrew Kulinich <github@4vanger.com>
2020-04-10 17:56:23 -0700

    infinite scroll

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,35 +1,71 @@
-import React, { useState } from 'react';
-import CardColumns from 'react-bootstrap/CardColumns';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 import { Card } from './Card';
-import { useEffect } from 'react';
+import './CardsList.scss';
 
 const CARDS_PER_REQUEST = 20;
+const SCROLL_THROTTLE_TIME_MS = 50;
 
 export function CardsList() {
+	const [isLoading, setIsLoading] = useState(false);
 	const [cards, setCards] = useState([]);
-	useEffect(() => fetchCards(setCards, 0, 0), []);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	useEffect(fetchCards, [pageNumber]);
+
+	useLayoutEffect(() => {
+		let timeoutId;
+		const handleScrollEvent = () => {
+			if (timeoutId) {
+				clearTimeout(timeoutId);
+			}
+			timeoutId = setTimeout(
+				checkScrollPosition,
+				SCROLL_THROTTLE_TIME_MS
+			);
+		};
+
+		window.addEventListener('scroll', handleScrollEvent);
+		return () => {
+			window.removeEventListener('scroll', handleScrollEvent);
+			clearTimeout(timeoutId);
+		};
+	});
+
+	function checkScrollPosition() {
+		const lastCardEl = document.querySelector(
+			'.CardsList .Card:last-child'
+		);
+
+		var elOffset = lastCardEl.offsetTop;
+		var pageOffset = window.pageYOffset + window.innerHeight;
+		if (elOffset - 200 < pageOffset) {
+			// load next page
+			setPageNumber(pageNumber + 1);
+		}
+	}
+
+	function fetchCards() {
+		setIsLoading(true);
+		fetch(
+			`https://api.elderscrollslegends.io/v1/cards?pageSize=${CARDS_PER_REQUEST}&page=${pageNumber}`
+		)
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((data) => {
+				setCards([...cards, ...data.cards]);
+				setIsLoading(false);
+			});
+	}
 
 	return (
 		<div className="CardsList">
-			<CardColumns>
-				{cards.map((card) => (
-					<Card card={card} key={card.id} />
-				))}
-			</CardColumns>
+			{isLoading && <Spinner animation="grow" />}
+			{cards.map((card) => (
+				<Card card={card} key={card.id} />
+			))}
 		</div>
 	);
-}
-
-function fetchCards(setCards, offset, page) {
-	fetch(
-		`https://api.elderscrollslegends.io/v1/cards?pageSize=${CARDS_PER_REQUEST}&page=${page}`
-	)
-		.then((resp) => {
-			return resp.json();
-		})
-		.then((data) => {
-			console.log(data);
-			setCards(data.cards);
-		});
 }
